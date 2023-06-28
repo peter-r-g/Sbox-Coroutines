4ea8cabc0fe52a83bb7d6edd516dd75d7876d91e
peter-r-g <petergorman11@gmail.com>
2023-06-28 15:49:07 -0400

    Add WaitForTask staller

---
+++ Stallers/WaitForTask.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+namespace Coroutines.Stallers;
+
+/// <summary>
+/// Pauses a coroutine until an asynchronous task completes.
+/// </summary>
+public sealed class WaitForTask : ICoroutineStaller
+{
+	/// <inheritdoc/>
+	public bool IsComplete => Task.IsCompleted;
+	/// <inheritdoc/>
+	public ExecutionStrategy ExecutionStrategy => ExecutionStrategy.Tick;
+
+	/// <summary>
+	/// The task to wait for.
+	/// </summary>
+	private Task Task { get; }
+
+	/// <summary>
+	/// Initializes a new instance of <see cref="WaitForTask"/>.
+	/// </summary>
+	/// <param name="task">The task to wait for.</param>
+	public WaitForTask( Task task )
+	{
+		Task = task;
+	}
+
+	/// <inheritdoc/>
+	public void Update()
+	{
+	}
+}
