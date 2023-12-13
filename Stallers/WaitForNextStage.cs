cc60ac17b57e89a5e043efaaf7ba43a252d808fb
peter-r-g <petergorman11@gmail.com>
2023-12-12 20:21:08 -0500

    Add WaitForNextStage

---
+++ Stallers/WaitForNextStage.cs
@@ -0,0 +1,28 @@
+﻿using static Sandbox.GameObjectSystem;
+
+namespace Coroutines.Stallers;
+
+/// <summary>
+/// Pauses a coroutine until the next execution of a <see cref="Stage"/>.
+/// </summary>
+public sealed class WaitForNextStage : ICoroutineStaller
+{
+	/// <inheritdoc/>
+	public bool IsComplete { get; private set; }
+	/// <inheritdoc/>
+	public Stage PollingStage { get; }
+
+	/// <summary>
+	/// Initializes a new instance of <see cref="WaitForNextStage"/>.
+	/// </summary>
+	public WaitForNextStage( Stage stage )
+	{
+		PollingStage = stage;
+	}
+
+	/// <inheritdoc/>
+	public void Update()
+	{
+		IsComplete = true;
+	}
+}
